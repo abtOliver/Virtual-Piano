5b83b616a9cd9c69d3f2d298b0b573f5820459e2
Oliver G. Abt <abto@lars-net.de>
2021-12-20 22:55:23 +0100

    Stage 4/5

--- Virtual Piano/task/src/script.js
+++ Virtual Piano/task/src/script.js
@@ -1,4 +1,9 @@
 document.addEventListener("keypress", function (e) {
+    function play(key) {
+        let aSound = new Audio("resources/" + key + ".mp3");
+        aSound.play();
+    }
+
     switch (e.code) {
         case "KeyA":
         case "KeyS":
@@ -7,6 +12,7 @@ document.addEventListener("keypress", function (e) {
         case "KeyG":
         case "KeyH":
         case "KeyJ":
+            play(e.key.toUpperCase());
             console.log("The '" + e.key.toUpperCase() + "' key is pressed.");
             break
         default:
